9bb9ca053aeb5c9551d0dec5c56b72d7672f13c0
Maxime Rodier <maxime.rodier.95@gmail.com>
2016-02-24 19:18:52 -0500

    Amélioration de l'affichage d'une scène vers l'affichage par buffer

--- S2_Projet/BasicScene.cpp
+++ S2_Projet/BasicScene.cpp
@@ -5,36 +5,25 @@
 
 BasicScene::BasicScene() : Scene()
 {
-
+	buffer = new char*[HEIGHT];
+	for(int i = 0; i < HEIGHT; i++)
+	{
+		buffer[i] = new char[WIDTH];
+		for(int j = 0; j < WIDTH; j++)
+			buffer[i][j] = ' ';
+	}
 }
 
 void BasicScene::afficher()
 {	
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-	
-
-	std::cout << "________________________________________________________________________________" << std::endl;
+	clear();
+	buffer[10][38] = 'M';
+	buffer[10][39] = 'A';
+	buffer[10][40] = 'X';
+	buffer[10][41] = 'I';
+	buffer[10][42] = 'M';
+	buffer[10][43] = 'E';
+	draw();
 }
 
 void BasicScene::input(float frameTime)
@@ -45,4 +34,31 @@ void BasicScene::input(float frameTime)
 Scene* BasicScene::update(float frameTime)
 {
 	return this;
+}
+
+void BasicScene::clear()
+{
+	int i = 0;
+	for(; i < HEIGHT; i++)
+	{
+		int j = 0;
+		for(; j < WIDTH; j++)
+		{
+			buffer[i][j] = ' ';
+		}
+	}
+}
+
+void BasicScene::draw()
+{
+	int i = 0;
+	for(; i < HEIGHT; i++)
+	{
+		int j = 0;
+		for(; j < WIDTH; j++)
+		{
+			std::cout << buffer[i][j];
+		}
+		std::cout << std::endl;
+	}
 }
